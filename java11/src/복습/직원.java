08406cb51351f846b3da1625336849b15bc1cb43
5ein <124111027+5ein@users.noreply.github.com>
2023-02-20 20:24:34 +0900

    Update 직원.java

--- java11/src/복습/직원.java
+++ java11/src/복습/직원.java
@@ -3,7 +3,7 @@ package 복습;
 public class 직원 {
 	public String name; // 같은 + 다른 패키지 (아무데서나 접근 가능)
 	String address; // 같은 패키지 접근 가능
-	protected int salsry; // 같은 패키지 + 상속한 다른 패키지 접근가능
+	protected int salry; // 같은 패키지 + 상속한 다른 패키지 접근가능
 	private String rrn; // 이 클래스 안에서만 접근가능
 	
 	
