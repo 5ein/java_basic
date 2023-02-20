4ff587647bbc23c30e87c312463c7b40b43e114e
5ein <124111027+5ein@users.noreply.github.com>
2023-02-20 20:24:46 +0900

    Update 직원사용.java

--- java11/src/복습/직원사용.java
+++ java11/src/복습/직원사용.java
@@ -5,7 +5,7 @@ public class 직원사용 {
 		직원 work = new 직원();
 		work.name = "홍길동"; //public
 		work.address = "강남구"; //protected
-		work.salsry = 100;//default
+		work.salary = 100;//default
 		
 				
 	}
