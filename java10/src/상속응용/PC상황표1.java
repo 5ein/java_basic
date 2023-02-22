f68ea669dcdcbb13312acf7fde1ac3c63c42bf6d
5ein <124111027+5ein@users.noreply.github.com>
2023-02-22 18:01:12 +0900

    Add files via upload

---
+++ java10/src/상속응용/PC상황표1.java
@@ -0,0 +1,15 @@
+package 상속응용;
+
+public class PC상황표1 {
+
+	public static void main(String[] args) {
+		타이머1 time = new 타이머1();
+		카운트1 count = new 카운트1();
+		이미지1 img = new 이미지1();
+		
+		time.start();
+		count.start();
+		img.start();
+	}
+
+}
---
+++ java10/src/상속응용/카운트1.java
@@ -0,0 +1,18 @@
+package 상속응용;
+
+public class 카운트1 extends Thread {
+	
+	@Override
+	public void run() {
+		for (int i = 500; i > 0; i--) {
+			System.out.println("카운트: " + i);
+			
+			try {
+				Thread.sleep(1000); // 1초 재우기
+			} catch (Exception e) {
+				System.out.println("cpu 연결에 문제가 생겼습니다.");
+			}
+		}
+	}
+	
+}
